1277beccb737952c15054783ccd1e8cb19890386
Majid Rafigh <majid.rafigh@absorblms.com>
2020-04-22 23:07:12 -0600

    Add mock api and node json server

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,44 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { connect } from "react-redux";
-import * as courseActions from "../../Redux/actions/courseActions";
+import * as courseActions from "../../redux/actions/courseActions";
 import PropTypes from 'prop-types'
 import {bindActionCreators} from 'redux';
 class CoursesPages extends React.Component {
-  state = {
-    course: {
-      title: "",
-    },
-  };
-
-  //Arrow functions inherit the binding context of their enclosing scope
-  handleChange = (event) => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    //dispatch is available in props as we left the mapDispatch method in connect method
-    //this.props.dispatch(courseActions.createCourse(this.state.course));
-    this.props.actions.createCourse(this.state.course);
-  };
-
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      //add a fragment to wrap the render, otherwise we will have two top level elements
+      <Fragment>
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type="text"
-          value={this.state.course.title}
-          onChange={this.handleChange} //bind the this of function to the this of the class
-        />
-        <input type="submit" value="Save" />
-
         {this.props.courses.map(course=>(
           <div key={course.title}>{course.title}</div>
         ))}
-      </form>
+        </Fragment>
     );
   }
 }
