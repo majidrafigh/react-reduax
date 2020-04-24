95553450d535b44e0c0bf9cf1b3f6d2691cdd90f
Majid Rafigh <majid.rafigh@absorblms.com>
2020-04-23 23:46:57 -0600

    Add manage course page

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,8 @@ import HomePage from "./home/HomePage";
 import AboutPage from "./about/About";
 import Header from "./common/Header";
 import PageNotFound from "./PageNotFound";
-import CoursesPages from "./courses/CoursesPage";
+import CoursesPage from "./courses/CoursesPage";
+import ManageCoursePage from "./courses/manageCoursePage"
 
 function App() {
   return (
@@ -13,7 +14,9 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
-        <Route path="/courses" component={CoursesPages} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/course" component={ManageCoursePage} />
         <Route component={PageNotFound} />
       </Switch>
     </div>
---
+++ src/components/courses/manageCoursePage.js
@@ -0,0 +1,51 @@
+import React, { Fragment } from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import { loadAuthors } from "../../redux/actions/authorsActions";
+import PropTypes from "prop-types";
+
+class ManageCoursePages extends React.Component {
+  componentDidMount() {
+    const { courses, authors, loadAuthors, loadCourses } = this.props;
+
+    if (courses.length === 0) {
+      loadCourses().catch((error) => {
+        alert("Loading courses failed:" + error);
+      });
+    }
+    if (authors.length === 0) {
+      loadAuthors().catch((error) => {
+        alert("Loading authors failed:" + error);
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h2>Manage Course</h2>
+      </Fragment>
+    );
+  }
+}
+
+ManageCoursePages.propTypes = {
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+    authors: state.authors,
+  };
+}
+
+const mapDispatchToProps = {
+  loadCourses: courseActions.loadCourses, //we can use named imports to make this shorter (check load authors)
+  loadAuthors, //confusion: we have two variables with the same name in this fie, the import and the action
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePages);
