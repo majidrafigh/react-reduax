3e62bb4a8789fcfbcaaa5bdc72cabbc368458afa
Majid Rafigh <majid.rafigh@absorblms.com>
2020-04-29 20:17:18 -0600

    Add delete functionality

--- src/Redux/actions/courseActions.js
+++ src/Redux/actions/courseActions.js
@@ -1,49 +1,62 @@
 import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi";
-import {beginApiCall} from "./apiStatusActions";
+import { beginApiCall } from "./apiStatusActions";
 
 export function loadCoursesSuccess(courses) {
-  return { type: types.LOAD_COURSES_SUCCESS, courses };
+    return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
 export function createCoursesSuccess(course) {
-  return { type: types.CREATE_COURSE_SUCCESS, course };
+    return { type: types.CREATE_COURSE_SUCCESS, course };
 }
 
 export function updateCoursesSuccess(course) {
-  return { type: types.UPDATE_COURSE_SUCCESS, course };
+    return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
+export function deleteCourseOptimistic(course) {
+    return { type: types.DELETE_COURSE_OPTIMISTIC, course };
 }
 
 //first thunk, put thunks in the bottom of the actions
 //every thunk returns a function that accepts a dispatch
 export function loadCourses() {
-  return function (dispatch) {
-    dispatch(beginApiCall());
-    //dispatch is injected by thunk middleware
-    return courseApi
-      .getCourses()
-      .then((coursesFromApi) => {
-        dispatch(loadCoursesSuccess(coursesFromApi));        
-        //dispatch({ type: types.LOAD_COURSES_SUCCESS, coursesFromApi });
-      })
-      .catch((error) => {
-        throw error;
-      });
-  };
+    return function (dispatch) {
+        dispatch(beginApiCall());
+        //dispatch is injected by thunk middleware
+        return courseApi
+            .getCourses()
+            .then((coursesFromApi) => {
+                dispatch(loadCoursesSuccess(coursesFromApi));
+                //dispatch({ type: types.LOAD_COURSES_SUCCESS, coursesFromApi });
+            })
+            .catch((error) => {
+                throw error;
+            });
+    };
 }
 
 export function saveCourse(course) {
-  return function (dispatch, getState) {
-    dispatch(beginApiCall());
-    return courseApi
-      .saveCourse(course)
-      .then((savedCourse) => {
-        course.id
-        ? dispatch(updateCoursesSuccess(savedCourse))
-        : dispatch(createCoursesSuccess(savedCourse));
-      })
-      .catch((error) => {
-        throw error;
-      });
-  };
+    return function (dispatch) {
+        dispatch(beginApiCall());
+        return courseApi
+            .saveCourse(course)
+            .then((savedCourse) => {
+                course.id
+                    ? dispatch(updateCoursesSuccess(savedCourse))
+                    : dispatch(createCoursesSuccess(savedCourse));
+            })
+            .catch((error) => {
+                throw error;
+            });
+    };
+}
+
+export function deleteCourse(course) {
+    return function (dispatch) {
+        // Doing optimistic delete, so not dispatching begin/end api call
+        // actions, or apiCallError action since we're not showing the loading status for this.
+        dispatch(deleteCourseOptimistic(course));
+        return courseApi.deleteCourse(course.id);
+    };
 }
--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -7,87 +7,117 @@ import { bindActionCreators } from "redux";
 import CourseList from "./courseList";
 import { Redirect } from "react-router-dom";
 import Spinner from "../common/Spinner";
+import { toast } from "react-toastify";
 
 class CoursesPages extends React.Component {
-  state = {
-    redirectToAddCoursePage: false,
-  };
+    state = {
+        redirectToAddCoursePage: false,
+    };
 
-  componentDidMount() {
-    const { courses, authors, actions } = this.props;
+    componentDidMount() {
+        const { courses, authors, actions } = this.props;
 
-    if (courses.length === 0) {
-      actions.loadCourses().catch((error) => {
-        alert("Loading courses failed:" + error);
-      });
+        if (courses.length === 0) {
+            actions.loadCourses().catch((error) => {
+                alert("Loading courses failed:" + error);
+            });
+        }
+        if (authors.length === 0) {
+            actions.loadAuthors().catch((error) => {
+                alert("Loading authors failed:" + error);
+            });
+        }
     }
-    if (authors.length === 0) {
-      actions.loadAuthors().catch((error) => {
-        alert("Loading authors failed:" + error);
-      });
+
+    handleDeleteCourse = async (course) => {
+        toast.success("Course deleted");
+        try {
+            await this.props.actions.deleteCourse(course);
+        } catch (error) {
+            toast.error("Delete failed. " + error.message, {
+                autoClose: false,
+            });
+        }
+    };
+
+    render() {
+        return (
+            //add a fragment to wrap the render, otherwise we will have two top level elements
+            <Fragment>
+                {this.state.redirectToAddCoursePage && (
+                    <Redirect to="/course" />
+                )}
+                <h2>Courses</h2>
+
+                {this.props.loading ? (
+                    <Spinner />
+                ) : (
+                    <>
+                        <button
+                            style={{ marginBottom: 20 }}
+                            className="btn btn-primary add-course"
+                            onClick={() =>
+                                this.setState({ redirectToAddCoursePage: true })
+                            }
+                        >
+                            Add Course
+                        </button>
+                        <CourseList
+                            courses={this.props.courses}
+                            onDeleteClick={this.handleDeleteCourse}
+                        />
+                    </>
+                )}
+            </Fragment>
+        );
     }
-  }
-
-  render() {
-    return (
-      //add a fragment to wrap the render, otherwise we will have two top level elements
-      <Fragment>
-        {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
-        <h2>Courses</h2>
-
-        {this.props.loading ? (
-          <Spinner />
-        ) : (
-          <>
-            <button
-              style={{ marginBottom: 20 }}
-              className="btn btn-primary add-course"
-              onClick={() => this.setState({ redirectToAddCoursePage: true })}
-            >
-              Add Course
-            </button>
-            <CourseList courses={this.props.courses} />
-          </>
-        )}
-      </Fragment>
-    );
-  }
 }
 
 CoursesPages.propTypes = {
-  actions: PropTypes.object.isRequired,
-  courses: PropTypes.array.isRequired,
-  authors: PropTypes.array.isRequired,
-  loading: PropTypes.bool.isRequired,
+    actions: PropTypes.object.isRequired,
+    courses: PropTypes.array.isRequired,
+    authors: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
 };
 
 //function mapStateToProps(state, ownProps) {
 function mapStateToProps(state) {
-  return {
-    courses:
-      state.authors.length === 0
-        ? [] //as we read authors and courses async, so we don't know if authors are ready, so we return an empty array
-        : state.courses.map((course) => {
-            return {
-              ...course,
-              authorName: state.authors.find((a) => a.id === course.authorId)
-                .name,
-            };
-          }),
-    authors: state.authors,
-    loading: state.apiCallInProgress > 0,
-  };
+    return {
+        courses:
+            state.authors.length === 0
+                ? [] //as we read authors and courses async, so we don't know if authors are ready, so we return an empty array
+                : state.courses.map((course) => {
+                      return {
+                          ...course,
+                          authorName: state.authors.find(
+                              (a) => a.id === course.authorId
+                          ).name,
+                      };
+                  }),
+        authors: state.authors,
+        loading: state.apiCallInProgress > 0,
+    };
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    // createCourse: course => dispatch(courseActions.createCourse(course))
-    //dispatch is the method that notifies the store about a new action
-    actions: {
-      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
-      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
-    },
-  };
+    return {
+        // createCourse: course => dispatch(courseActions.createCourse(course))
+        //dispatch is the method that notifies the store about a new action
+        actions: {
+            loadCourses: bindActionCreators(
+                courseActions.loadCourses,
+                dispatch
+            ),
+            loadAuthors: bindActionCreators(
+                authorActions.loadAuthors,
+                dispatch
+            ),
+            deleteCourse: bindActionCreators(
+                courseActions.deleteCourseOptimistic,
+                dispatch
+            ),
+        },
+    };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPages);
